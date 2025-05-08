c0a58f091d55ff282f78717efb1c9d4325d09095
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2025-05-08 10:21:09 +0000

    Create main.tf

---
+++ modules/main.tf
@@ -0,0 +1,22 @@
+# Create Aws connector using irsa Authentication
+resource "harness_platform_connector_aws" "this" {
+  for_each = { for connector in var.connectors : connector.identifier => connector }
+
+  identifier  = each.value.identifier
+  name        = each.value.name
+  description = each.value.description
+  org_id      = each.value.org_id
+  project_id  = each.value.project_id
+
+  cross_account_access {
+    role_arn = each.value.cross_account_access_role_arn
+    # external_id = ""
+  }
+
+  irsa {
+    delegate_selectors = ["${each.value.harnessdelegate}"]
+    region             = each.value.region
+  }
+
+  tags = var.tags
+}
