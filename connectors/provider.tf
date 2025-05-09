1e221c144eaa4722c6f3fdae154fd5dbd0d04986
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2025-05-09 15:13:13 +0000

    Update provider.tf

--- connectors/provider.tf
+++ connectors/provider.tf
@@ -3,4 +3,10 @@ provider "harness" {
   account_id       = var.TF_VAR_HARNESS_ACCOUNT_ID
   platform_api_key = var.TF_VAR_HARNESS_PLATFORM_API_KEY
 }
+provider "azurerm" {
+  alias         = "rbac"
+  client_id     = var.rbac_sp_client_id
+  client_secret = var.rbac_sp_client_secret
+  features {}
+}
 
