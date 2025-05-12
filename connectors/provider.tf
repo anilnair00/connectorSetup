70e870a7dffd32e05ab1cb138cf61e5e10fb126d
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2025-05-12 14:20:41 +0000

    Update provider.tf

--- connectors/provider.tf
+++ connectors/provider.tf
@@ -6,7 +6,7 @@ provider "harness" {
 provider "azurerm" {
   use_oidc        = true
   client_id       = "084f318f-40aa-4d40-93ea-5ed3472b66bc"
-  client_secret   = "UxS8Q~xZ6gqVHJlJlDbvM~-3BpEWwMmFpetXaaLD"
+# client_secret   = "UxS8Q~xZ6gqVHJlJlDbvM~-3BpEWwMmFpetXaaLD"
 #  tenant_id       = "abec7981-3822-4685-98b0-533aca20c2ed"
 #  subscription_id = "ee902f6d-01a9-4a32-8c5f-af9403e6c766"
   features {}
