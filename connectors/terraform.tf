fb383061c71a7d6247e93ea4bafe01409ab169dd
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2025-05-12 12:43:20 +0000

    Update terraform.tf

--- connectors/terraform.tf
+++ connectors/terraform.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~>2.0.3"
+  required_version = "~>3.0"
   backend "azurerm" {
     resource_group_name  = "Testvm_group"
     storage_account_name = "testvmgroupb7e2"
