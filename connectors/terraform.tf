08d2028c83ecc8ec865a9237f6eb8568576eed38
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2025-05-15 11:05:31 +0000

    Update terraform.tf

--- connectors/terraform.tf
+++ connectors/terraform.tf
@@ -1,21 +1,16 @@
-terraform {
-  required_version = ">=1.9"
-  backend "azurerm" {
-    use_oidc             = true                                    # Can also be set via `ARM_USE_OIDC` environment variable.
-    use_azuread_auth     = true                                    # Can also be set via `ARM_USE_AZUREAD` environment variable.
-    resource_group_name  = "Testvm_group"
-    storage_account_name = "testvmgroupb7e2"
-    container_name       = "tfstate"
-    key                  = "test.tfstate"
-  }
-  required_providers {
-    harness = {
-      source = "harness/harness"
-      version = "0.24.2"  
-    }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=4.1.0"
-    }
+terraform {  
+    required_providers {  
+        harness = {  
+            source = "harness/harness"  
+            version = "0.24.2"  
+        }  
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "~>3.0"
+        }
+    }  
+    backend "azurerm" {
+      use_oidc         = true
+      use_azuread_auth = true
   }
 }
