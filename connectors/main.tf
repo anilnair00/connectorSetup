7f215bba38ee51a7ede2b2d9a2934a8a766d891c
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2025-05-15 18:09:15 +0000

    Update main.tf

--- connectors/main.tf
+++ connectors/main.tf
@@ -6,7 +6,18 @@ locals {
   env_cross_account_role_arn_map = zipmap(local.envs, local.cross_account_role_arns)
 
   default_tags_list = [
-    "Test:0000",
+    "CostCode/AFC:1904",
+    "Tower:operation",
+    "DepartmentID:1904",
+    "DepartmentName:ops-digital",
+    "ProjectName:Notification",
+    "Application:ACNP",
+    "TechOwner:Azeem Qaiser",
+    "BusinessOwner:Pradeep Nishantha",
+    "Criticality:Critical",
+    "Sensitivity:High",
+    "ManagedBy:Terraform",
+    "tf-repo-name:ac-it-notifications-harness-resources-tf"
   ]
 
   connectors = flatten([
