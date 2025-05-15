52b5db47b41166d889c86f6847b237cc69b6a2fb
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2025-05-15 11:03:50 +0000

    Update provider.tf

--- connectors/provider.tf
+++ connectors/provider.tf
@@ -4,11 +4,7 @@ provider "harness" {
   platform_api_key = var.TF_VAR_HARNESS_PLATFORM_API_KEY
 }
 provider "azurerm" {
+  features {}
   use_oidc        = true
-  client_id       = "084f318f-40aa-4d40-93ea-5ed3472b66bc"
-# client_secret   = "UxS8Q~xZ6gqVHJlJlDbvM~-3BpEWwMmFpetXaaLD"
-  tenant_id       = "abec7981-3822-4685-98b0-533aca20c2ed"
-#  subscription_id = "ee902f6d-01a9-4a32-8c5f-af9403e6c766"
-   features {}
 }
 
